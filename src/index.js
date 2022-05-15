61d84e5d2e4f2ea4146c6258d9c0940102c42539
blevinspercussion <blevinspercussion@gmail.com>
2022-05-15 08:20:26 -0500

    changed color scheme. Added welcome screen. created some of the framework for index.js

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// Global variables
+const practiceTab = document.getElementById('practice-tab');
+const shortTermTab = document.getElementById('short-term-tab');
+const longTermTab = document.getElementById('long-term-tab');
+
+
+
+//Event listeners
+
+
+
+// Factory function for goals
+
+
+
+
+// Function for Daily Practice 
+
