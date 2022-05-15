d93ac53304187194d2a0f7f9e61404a948ff7bf1
blevinspercussion <blevinspercussion@gmail.com>
2022-05-15 08:21:58 -0500

    created global variables for main DOM elements in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const practiceTab = document.getElementById('practice-tab');
 const shortTermTab = document.getElementById('short-term-tab');
 const longTermTab = document.getElementById('long-term-tab');
 
+const sidebar = document.querySelector('.sidebar');
+const mainContent = document.querySelector('.main-content');
+
 
 
 //Event listeners
