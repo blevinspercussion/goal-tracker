c1d7e2a0d6cdebd2e7babe711f30eaa52d7c9112
blevinspercussion <blevinspercussion@gmail.com>
2022-05-16 08:43:26 -0500

    added form to add daily practice, still need to wire it in though

--- src/index.js
+++ src/index.js
@@ -58,11 +58,62 @@ const tabEventListeners = (() => {
             daysList.appendChild(dayItem);
 
         };
+
+        dailyPractice();
+
+        return { addPracticeButton };
     });
 });
 
 // Daily Practice Module
 const dailyPractice = (() => {
+
+    const dailyGoal = (title, description, type='daily', time) => {
+        return {title, description, type, time};
+    }
+
+    const addPracticeButton = document.getElementById('add-practice-button');
+    let days = document.querySelectorAll('.days-list-item');
+
+    
+
+    addPracticeButton.addEventListener('click', () => {
+        clearMainContent();
+
+        let addPracticeFormHead = document.createElement('h1');
+        let addPracticeForm = document.createElement('form');
+        let titleLabel = document.createElement('label');
+        let titleField = document.createElement('input');
+        let descriptionLabel = document.createElement('label');
+        let descriptionField = document.createElement('input');
+        let timeLabel = document.createElement('label');
+        let timeField = document.createElement('input');
+        let addButton = document.createElement('button');
+
+        titleLabel.textContent = 'Title';
+        descriptionLabel.textContent = 'Description';
+        timeLabel.textContent = 'Time (in minutes)';
+        addButton.textContent = 'Add Practice';
+
+        titleField.setAttribute('type', 'text');
+        descriptionField.setAttribute('type', 'text');
+        descriptionField.setAttribute('rows', '10');
+        descriptionField.setAttribute('cols', '20');
+        timeField.setAttribute('type', 'number');
+
+        mainContent.appendChild(addPracticeForm);
+        addPracticeForm.appendChild(titleLabel);
+        addPracticeForm.appendChild(titleField);
+        addPracticeForm.appendChild(descriptionLabel);
+        addPracticeForm.appendChild(descriptionField);
+        addPracticeForm.appendChild(timeLabel);
+        addPracticeForm.appendChild(timeField);
+        addPracticeForm.appendChild(addButton);
+
+
+    });
+
+    
 
 });
 
