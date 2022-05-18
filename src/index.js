9b409ac0330c4eb60e4dc8adc20352a28829851c
blevinspercussion <blevinspercussion@gmail.com>
2022-05-18 09:37:20 -0500

    restarted index.js file. added display controller with functions to clear the sidebar and main content divs

--- src/index.js
+++ src/index.js
@@ -1,376 +1,41 @@
-// Global variables
-const practiceTab = document.getElementById('practice-tab');
-const shortTermTab = document.getElementById('short-term-tab');
-const longTermTab = document.getElementById('long-term-tab');
+// Global DOM Elements
+const mainContentDiv = document.querySelector('.main-content');
+const headerDiv = document.querySelector('.header');
+const sidebarDiv = document.querySelector('.sidebar');
+const tabsDiv = document.querySelector('.tabs');
 
-const sidebar = document.querySelector('.sidebar');
-const mainContent = document.querySelector('.main-content');
+const dailyPracticeTab = document.getElementById('practice-tab');
+const shortTermGoalsTab = document.getElementById('short-term-tab');
+const longTermGoalsTab = document.getElementById('long-term-tab');
 
-let dailyGoals = [];
-let shortTermGoals = [];
-let longTermGoals = [];
-
-// Goal factory functions
-
-const dailyGoal = (title, type, description, time) => {
-    return {title, type, description, time};
-}
-
-const shortTermGoal = (title, description, type='short term', dueDate) => {
-    return {title, description, type, dueDate};
-};
-
-const longTermGoal = (title, description, type='long term') => {
-    return {title, description, type};
-};
-
-
-// Display controller, handles function for displaying
-// and clearing content to/from the screen
-const displayController = () => {
-
-    function clearMainContent() {
-        while (mainContent.firstChild) {
-            mainContent.removeChild(mainContent.firstChild);
-        };
-    };
-    
-    function clearSidebar() {
-        while (sidebar.firstChild) {
-            sidebar.removeChild(sidebar.firstChild);
+// Display controller handles adding elements to, and removing 
+// elements from, the DOM
+const displayController = (() => {
+    const clearMainContent = () => {
+        while (mainContentDiv.firstChild) {
+            mainContentDiv.removeChild(mainContentDiv.firstChild);
         };
     };
 
-    return { clearMainContent, clearSidebar };
-
-};
-
-
-// Global functions
-
-
-
-// Set up event listeners for tabs
-const tabEventListeners = (() => {
-
-    practiceTab.addEventListener('click', () => {
-
-        clearMainContent();
-        clearSidebar();
-
-        const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-        const daysDiv = document.createElement('div');
-        const daysList = document.createElement('ul');
-        const daysWelcomeDiv = document.createElement('div');
-        const daysWelcomeText = document.createElement('p');
-        const addPracticeButton = document.createElement('button');
-
-
-        daysDiv.setAttribute('id', 'days-div');
-        daysList.setAttribute('id', 'days-list');
-        daysWelcomeDiv.classList.add('welcome');
-        addPracticeButton.setAttribute('id', 'add-practice-button');
-        
-        sidebar.appendChild(daysDiv);
-        daysDiv.appendChild(daysList);
-        sidebar.appendChild(addPracticeButton);
-
-        mainContent.appendChild(daysWelcomeDiv);
-        daysWelcomeDiv.appendChild(daysWelcomeText);
-
-
-        daysWelcomeText.textContent = 'Click on a day at the left to view your practice goals for that day.';
-        addPracticeButton.textContent = 'Add Practice';
-
-        for (let day in days) {
-            let dayItem = document.createElement('li');
-            dayItem.textContent = `${days[day]}`;
-            dayItem.classList.add('days-list-item');
-            dayItem.setAttribute('id', dayItem.textContent);
-            daysList.appendChild(dayItem);
-
+    const clearSidebar = () => {
+        while (sidebarDiv.firstChild) {
+            sidebarDiv.removeChild(sidebarDiv.firstChild);
         };
+    };
 
-        dailyPractice();
+    const drawGoalCard = () => {
+        // TODO
+    };
 
-        return { addPracticeButton };
-    });
+    return {clearMainContent, clearSidebar, drawGoalCard};
+
+})();
+
+///////////////////////////////////
+////// Event Listeners for Tabs ///
+///////////////////////////////////
+
+dailyPracticeTab.addEventListener('click', () => {
+    displayController.clearMainContent();
 });
 
-// Daily Practice Module
-const dailyPractice = (() => {
-
-    
-
-    const addPracticeButton = document.getElementById('add-practice-button');
-    let days = document.querySelectorAll('.days-list-item');
-
-    // TODO Fills in details on practice screen
-
-
-
-
-    addPracticeButton.addEventListener('click', () => {
-        clearMainContent();
-
-        let addPracticeFormHead = document.createElement('h1');
-        let addPracticeForm = document.createElement('form');
-        let titleLabel = document.createElement('label');
-        let titleField = document.createElement('input');
-        let typeLabel = document.createElement('label')
-        let typeField = document.createElement('select');
-        let optionWarmup = document.createElement('option');
-        let optionTechnique = document.createElement('option');
-        let optionReading = document.createElement('option');
-        let optionRepertoire = document.createElement('option');
-        let optionOther = document.createElement('option');
-        let descriptionLabel = document.createElement('label');
-        let descriptionField = document.createElement('input');
-        let timeLabel = document.createElement('label');
-        let timeField = document.createElement('input');
-        let submitPracticeButton = document.createElement('button');
-
-        addPracticeFormHead.textContent = 'Add New Practice';
-        titleLabel.textContent = 'Title';
-        typeLabel.textContent = 'Type';
-        descriptionLabel.textContent = 'Description';
-        timeLabel.textContent = 'Time (in minutes)';
-        submitPracticeButton.textContent = 'Submit';
-
-        optionWarmup.textContent = 'Warmup';
-        optionTechnique.textContent = 'Technique';
-        optionReading.textContent = 'Reading';
-        optionRepertoire.textContent = 'Repertoire';
-        optionOther.textContent = 'Other';
-
-        addPracticeForm.setAttribute('onsubmit', 'return false'); // Prevents page from reloading
-        titleField.setAttribute('type', 'text');
-        optionWarmup.setAttribute('value', 'Warmup');
-        optionTechnique.setAttribute('value', 'Technique');
-        optionReading.setAttribute('value', 'Reading');
-        optionRepertoire.setAttribute('value', 'Repertoire');
-        optionOther.setAttribute('value', 'Other');
-        descriptionField.setAttribute('type', 'text');
-        descriptionField.setAttribute('rows', '10');
-        descriptionField.setAttribute('cols', '20');
-        timeField.setAttribute('type', 'number');
-
-        mainContent.appendChild(addPracticeFormHead);
-        mainContent.appendChild(addPracticeForm);
-        addPracticeForm.appendChild(titleLabel);
-        addPracticeForm.appendChild(titleField);
-        addPracticeForm.appendChild(typeLabel);
-        addPracticeForm.appendChild(typeField);
-        typeField.appendChild(optionWarmup);
-        typeField.appendChild(optionTechnique);
-        typeField.appendChild(optionReading);
-        typeField.appendChild(optionRepertoire);
-        typeField.appendChild(optionOther);
-        addPracticeForm.appendChild(descriptionLabel);
-        addPracticeForm.appendChild(descriptionField);
-        addPracticeForm.appendChild(timeLabel);
-        addPracticeForm.appendChild(timeField);
-        addPracticeForm.appendChild(submitPracticeButton);
-
-
-        submitPracticeButton.addEventListener('click', () => {
-            practice = dailyGoal(titleField.value, typeField.value, descriptionField.value, timeField.value);
-            addPracticeForm.reset();
-            dailyGoals.push(practice);
-            console.log(dailyGoals);
-        });
-
-
-        return {submitPracticeButton};
-    });
-    
-    days.forEach(day => {
-        day.addEventListener('click', () => {
-            let dayText = day.textContent;
-            clearMainContent();
-
-            const warmupHeader = document.createElement('h1');
-            const techniqueHeader = document.createElement('h1');
-            const readingHeader = document.createElement('h1');
-            const repertoireHeader = document.createElement('h1');
-            const otherHeader = document.createElement('h1');
-
-            const warmupList = document.createElement('ul');
-            const techniqueList = document.createElement('ul');
-            const readingList = document.createElement('ul');
-            const repertoireList = document.createElement('ul');
-            const otherList = document.createElement('ul');
-
-            warmupList.classList.add('practice-list');
-            techniqueList.classList.add('practice-list');
-            readingList.classList.add('practice-list');
-            repertoireList.classList.add('practice-list');
-            otherList.classList.add('practice-list');
-
-            warmupList.setAttribute('id', 'Warmup');
-            techniqueList.setAttribute('id', 'Technique');
-            readingList.setAttribute('id', 'Reading');
-            repertoireList.setAttribute('id', 'Repertoire');
-            otherList.setAttribute('id', 'Other');
-
-            
-            warmupHeader.textContent = 'Warmup';
-            techniqueHeader.textContent = 'Technique';
-            readingHeader.textContent = 'Reading';
-            repertoireHeader.textContent = 'Repertoire';
-            otherHeader.textContent = 'Other';
-            
-
-            mainContent.appendChild(warmupHeader);
-            mainContent.appendChild(techniqueHeader);
-            mainContent.appendChild(readingHeader);
-            mainContent.appendChild(repertoireHeader);
-            mainContent.appendChild(otherHeader);
-
-            warmupHeader.appendChild(warmupList);
-            techniqueHeader.appendChild(techniqueList);
-            readingHeader.appendChild(readingList);
-            repertoireHeader.appendChild(repertoireList);
-            otherHeader.appendChild(otherList);
-
-            
-            for (let goal in dailyGoals) {
-                if (dailyGoals[goal].type === 'Warmup') {
-                    let title = document.createElement('ul');
-                    let description = document.createElement('li');
-                    let time = document.createElement('li');
-                    let removePracticeButton = document.createElement('button');
-
-                    title.classList.add('practice-title');
-                    description.classList.add('practice-list-item');
-                    time.classList.add('practice-list-item');
-
-                    title.textContent = dailyGoals[goal].title;
-                    description.textContent = `Notes: ${dailyGoals[goal].description}`;
-                    time.textContent = `Time: ${dailyGoals[goal].time} minutes`;
-                    removePracticeButton.textContent = 'Remove This Practice Item';
-
-                    warmupList.append(title);
-                    title.append(description);
-                    title.append(time);
-                    warmupList.appendChild(removePracticeButton);
-
-                    removePracticeButton.addEventListener('click', () => {
-                        dailyGoals.splice(goal, 1);
-                    });
-
-                } else if (dailyGoals[goal].type === 'Technique') {
-                    let title = document.createElement('ul');
-                    let description = document.createElement('li');
-                    let time = document.createElement('li');
-                    let removePracticeButton = document.createElement('button');
-
-                    title.classList.add('practice-title');
-                    description.classList.add('practice-list-item');
-                    time.classList.add('practice-list-item');
-                    removePracticeButton.textContent = 'Remove This Practice Item';
-
-                    title.textContent = dailyGoals[goal].title;
-                    description.textContent = `Notes: ${dailyGoals[goal].description}`;
-                    time.textContent = `Time: ${dailyGoals[goal].time} minutes`;
-
-                    techniqueList.append(title);
-                    title.append(description);
-                    title.append(time);
-                    techniqueList.appendChild(removePracticeButton);
-
-                    removePracticeButton.addEventListener('click', () => {
-                        dailyGoals.splice(goal, 1);
-                    });
-                    
-                } else if (dailyGoals[goal].type === 'Reading') {
-                    let title = document.createElement('ul');
-                    let description = document.createElement('li');
-                    let time = document.createElement('li');
-                    let removePracticeButton = document.createElement('button');
-
-                    title.classList.add('practice-title');
-                    description.classList.add('practice-list-item');
-                    time.classList.add('practice-list-item');removePracticeButton.textContent = 'Remove This Practice Item';
-
-                    title.textContent = dailyGoals[goal].title;
-                    description.textContent = `Notes: ${dailyGoals[goal].description}`;
-                    time.textContent = `Time: ${dailyGoals[goal].time} minutes`;
-
-                    readingList.append(title);
-                    title.append(description);
-                    title.append(time);
-                    readingList.appendChild(removePracticeButton);
-
-                    removePracticeButton.addEventListener('click', () => {
-                        dailyGoals.splice(goal, 1);
-                    });
-
-                } else if (dailyGoals[goal].type === 'Repertoire') {
-                    let title = document.createElement('ul');
-                    let description = document.createElement('li');
-                    let time = document.createElement('li');
-                    let removePracticeButton = document.createElement('button');
-
-                    title.classList.add('practice-title');
-                    description.classList.add('practice-list-item');
-                    time.classList.add('practice-list-item');
-                    removePracticeButton.textContent = 'Remove This Practice Item';
-
-                    title.textContent = dailyGoals[goal].title;
-                    description.textContent = `Notes: ${dailyGoals[goal].description}`;
-                    time.textContent = `Time: ${dailyGoals[goal].time} minutes`;
-
-                    repertoireList.append(title);
-                    title.append(description);
-                    title.append(time);
-                    repertoireList.appendChild(removePracticeButton);
-
-                    removePracticeButton.addEventListener('click', () => {
-                        dailyGoals.splice(goal, 1);
-                    });
-
-                } else if (dailyGoals[goal].type === 'Other') {
-                    let title = document.createElement('ul');
-                    let description = document.createElement('li');
-                    let time = document.createElement('li');
-                    let removePracticeButton = document.createElement('button');
-
-                    title.classList.add('practice-title');
-                    description.classList.add('practice-list-item');
-                    time.classList.add('practice-list-item');
-                    removePracticeButton.textContent = 'Remove This Practice Item';
-
-                    title.textContent = dailyGoals[goal].title;
-                    description.textContent = `Notes: ${dailyGoals[goal].description}`;
-                    time.textContent = `Time: ${dailyGoals[goal].time} minutes`;
-
-                    otherList.append(title);
-                    title.append(description);
-                    title.append(time);
-                    otherList.appendChild(removePracticeButton);
-
-                    removePracticeButton.addEventListener('click', () => {
-                        dailyGoals.splice(goal, 1);
-                    });
-                };
-            };
-
-        });
-    });
-
-});
-
-
-// TODO Short term goals module 
-
-
-
-// TODO Long term goals module
-
-
-
-
-// Add event listeners 
-
-tabEventListeners();
