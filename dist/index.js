a4491f964972e98a129f78dfc3c69dfd999ec6b3
Adam Blevins <blevinspercussion@gmail.com>
2022-07-11 11:47:48 -0500

    Added firebase config to index.js

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDlrz1TElpSZh8CZRm8OOhGTJHMzwRQl-Y",
+    authDomain: "goaltracker-e0bc1.firebaseapp.com",
+    projectId: "goaltracker-e0bc1",
+    storageBucket: "goaltracker-e0bc1.appspot.com",
+    messagingSenderId: "1070489524132",
+    appId: "1:1070489524132:web:a8ba2c0a52fe9f8f3d1331"
+  };
+
+
 // Global DOM Elements
 const mainContentDiv = document.querySelector('.main-content');
 const headerDiv = document.querySelector('.header');
@@ -473,3 +486,6 @@ addGoalButton.addEventListener('click', () => {
     formController.displayAddGoalForm();
 
 });
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
