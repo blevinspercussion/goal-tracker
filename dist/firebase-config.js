4242e50ca51c5aa55088ffd70c8ac4b60de7cfb8
Adam Blevins <blevinspercussion@gmail.com>
2022-07-11 11:56:56 -0500

    Moved firebase config into its own module

---
+++ dist/firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDlrz1TElpSZh8CZRm8OOhGTJHMzwRQl-Y",
+    authDomain: "goaltracker-e0bc1.firebaseapp.com",
+    projectId: "goaltracker-e0bc1",
+    storageBucket: "goaltracker-e0bc1.appspot.com",
+    messagingSenderId: "1070489524132",
+    appId: "1:1070489524132:web:a8ba2c0a52fe9f8f3d1331"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,3 @@
-import { initializeApp } from "firebase/app";
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyDlrz1TElpSZh8CZRm8OOhGTJHMzwRQl-Y",
-    authDomain: "goaltracker-e0bc1.firebaseapp.com",
-    projectId: "goaltracker-e0bc1",
-    storageBucket: "goaltracker-e0bc1.appspot.com",
-    messagingSenderId: "1070489524132",
-    appId: "1:1070489524132:web:a8ba2c0a52fe9f8f3d1331"
-  };
-
-
 // Global DOM Elements
 const mainContentDiv = document.querySelector('.main-content');
 const headerDiv = document.querySelector('.header');
@@ -487,5 +474,3 @@ addGoalButton.addEventListener('click', () => {
 
 });
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
